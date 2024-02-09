52584727e02ca8b7dfa9e95c380fd414fb17ed96
theo-be <belliere.theo@outlook.fr>
2024-02-09 14:20:25 +0100

    Classe DebugInfo

---
+++ src/DebugInfo.cpp
@@ -0,0 +1,6 @@
+
+
+#include "DebugInfo.hpp"
+
+
+
---
+++ src/DebugInfo.hpp
@@ -0,0 +1,23 @@
+
+#ifndef DEBUGINFO_H
+#define DEBUGINFO_H
+
+
+#include <string>
+#include <vector>
+
+
+class DebugInfo {
+    public:
+
+
+
+    private:
+
+
+    std::string m_label;
+
+    std::vector<std::string> m_infos;
+};
+
+#endif
