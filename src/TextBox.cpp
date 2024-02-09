0177d5e0411da2324a54f82eeeead9cc19a9c953
theo-be <belliere.theo@outlook.fr>
2024-02-09 14:20:47 +0100

    Changement constructeur

--- src/TextBox.cpp
+++ src/TextBox.cpp
@@ -3,11 +3,11 @@
 #include <string>
 
 #include "TextBox.hpp"
-#include "constants.hpp"
+#include "Font.hpp"
 
 using namespace std;
 
-TextBox::TextBox() : message(""), speaker(""), used(false), messageIndex(-1), fontSize(FONT_SIZE_INTERFACE)
+TextBox::TextBox() : message(""), speaker(""), used(false), messageIndex(-1), fontSize(DEFAULT_FONT_SIZE)
 {}
 
 TextBox::TextBox(string message, string speaker, int index, int fontSize) : message(message), speaker(speaker), used(false), messageIndex(index), fontSize(fontSize)
