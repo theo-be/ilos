237ff37cae31fc5545210f3e99ca3b27e2bee7ae
theo-be <belliere.theo@outlook.fr>
2024-02-22 22:18:17 +0100

    Correction mineure

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,9 @@
  * @brief Fichier principal du projet
 */
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
-
 
 #include "main.hpp"
 #include "App.hpp"
-
-using namespace std;
 
 int main(int argc, char *argv[]) {
 
@@ -24,18 +19,4 @@ int main(int argc, char *argv[]) {
     App::quit();
 
     return 0;
-}
-
-
-/**
- * @fn void sleepUntil (const chrono::steady_clock::time_point &targetTime)
- * @param targetTime point temporel en microsecondes
- * @brief Met en pause l'execution du programme afin d'avoir de taux de rafraichissement 
-*/
-void sleepUntil (const chrono::steady_clock::time_point &targetTime) {
-    chrono::steady_clock::time_point now = chrono::steady_clock::now();
-    for (;;) {
-        now = chrono::steady_clock::now();
-        if (chrono::duration_cast<chrono::duration<double>>(targetTime - now).count() <= .0) return;
-    }
 }
--- src/main.hpp
+++ src/main.hpp
@@ -2,12 +2,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <SDL2/SDL.h>
-
-#include <chrono>
-
-
-void sleepUntil (const std::chrono::steady_clock::time_point &targetTime);
 
 #endif
 
